c1661d96ff5842ec20cb7dd6fbdd5ddddca863c9
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-02-17 15:44:17 +0900

    .envを使えるようにする設定や、migrationやseed関連のコマンドの追記

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+require("dotenv").config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      database: process.env.DB_NAME || "postgress",
+      user: process.env.DB_USER || "workout_log",
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+      directory: "knex_migrations",
+    },
+    seeds: { directory: "" },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
