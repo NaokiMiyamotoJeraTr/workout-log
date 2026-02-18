8be3bfa1c1be10af6842ff761fdbdd49d2cded27
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-02-18 14:32:13 +0900

    workoutの記録を取得するAPIの実装

--- app.js
+++ app.js
@@ -17,6 +17,28 @@ function buildApp() {
   // exercise種目登録のAPI
   // app.post("/exercises", (ree, res));
 
+  // worukoutの記録を表示するAPI
+  app.get("/workouts", async (req, res) => {
+    const data = await knex("workouts")
+      .join("sets", "workouts.id", "sets.workout_id")
+      .join("exercises", "sets.exercise_id", "exercises.id")
+      .select(
+        "workouts.id as workout_id",
+        "workouts.date",
+        "exercises.name as exercise",
+        "sets.weight",
+        "sets.reps",
+      );
+    const result = data.map((ele) => {
+      return {
+        ...ele,
+        date: ele["date"].toISOString().slice(0, 10), //日付処理の応急処置
+      };
+    });
+    // console.log("resultとは", result);
+    res.status(200).json({ workouts: result });
+  });
+
   // workoutの記録を保存するAPI
   // app.post("/workouts", (rewq, res));
 
